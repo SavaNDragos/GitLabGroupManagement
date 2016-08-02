21736df6d1f6e4731d66158e6d35b5a48c976b61
Sava Nicolae Dragos <sava.n.dragos@hotmail.com>
2016-08-03 00:58:49 +0100

    Add types for gitlab Json responses

---
+++ GitLabManagement/GitLabUserTypes.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLabManagement
+{
+
+    public class RootGitLabUsers
+    {
+        public GitLabUsers[] Property1 { get; set; }
+    }
+
+    public class GitLabUsers
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string name { get; set; }
+        public string state { get; set; }
+        public string avatar_url { get; set; }
+        public string web_url { get; set; }
+        public DateTime created_at { get; set; }
+        public bool is_admin { get; set; }
+        public object bio { get; set; }
+        public object location { get; set; }
+        public string skype { get; set; }
+        public string linkedin { get; set; }
+        public string twitter { get; set; }
+        public string website_url { get; set; }
+        public DateTime? last_sign_in_at { get; set; }
+        public DateTime confirmed_at { get; set; }
+        public int theme_id { get; set; }
+        public int color_scheme_id { get; set; }
+        public int projects_limit { get; set; }
+        public DateTime current_sign_in_at { get; set; }
+        public Identity[] identities { get; set; }
+        public bool can_create_group { get; set; }
+        public bool can_create_project { get; set; }
+        public bool two_factor_enabled { get; set; }
+        public bool external { get; set; }
+    }
+
+    public class Identity
+    {
+        public string provider { get; set; }
+        public string extern_uid { get; set; }
+    }
+
+
+    public class RootGitLabGroup
+    {
+        public GitLabGroup[] Property1 { get; set; }
+    }
+
+    public class GitLabGroup
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string path { get; set; }
+        public string description { get; set; }
+    }
+
+
+    public class RootUserPerGroup
+    {
+        public UserPerGroup[] Property1 { get; set; }
+    }
+
+    public class UserPerGroup
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string name { get; set; }
+        public string state { get; set; }
+        public DateTime created_at { get; set; }
+        public int access_level { get; set; }
+    }
+
+
+}
--- GitLabManagement/GitlabApiHelper.cs
+++ GitLabManagement/GitlabApiHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RestSharp;
+using RestSharp.Deserializers;
 
 namespace GitLabManagement
 {
@@ -12,8 +13,8 @@ namespace GitLabManagement
         public string GitLabAddress { get; set; }
         public string PrivateToken { get; set; }
         private string GitLabApi => $"{GitLabAddress}/{"api/v3"}";
-
-        private string ExecuteQuery(string inSpace, Dictionary<string, string> inSegments, RestSharp.Method inMethod)
+        
+        private IRestResponse ExecuteQuery(string inSpace, Dictionary<string, string> inSegments, RestSharp.Method inMethod)
         {
             try
             {
@@ -27,7 +28,7 @@ namespace GitLabManagement
                 }
 
                 var response = client.Execute(request);
-                return response.Content;
+                return response;
             }
             catch (Exception)
             {
@@ -36,17 +37,75 @@ namespace GitLabManagement
             }
         }
 
-        public void GetProjectGroupId(string inGroupName)
+        private List<GitLabUsers> GetAllUsers()
         {
-            var tempParameters = new Dictionary<string, string> {{"search", inGroupName}};
-            var result = ExecuteQuery("groups", tempParameters, Method.GET);
+            var tempParameters = new Dictionary<string, string>();
+            var result = ExecuteQuery("users", tempParameters, Method.GET);
+            var response2 = new JsonDeserializer().Deserialize<RootGitLabUsers>(result);
+            return response2.Property1.ToList();
         }
 
-        public void GetAllUsersForGroup(string inGroupName)
+        public GitLabGroup GetProjectGroupId(string inGroupName)
+        {
+            var returnGroupId = 0;
+            var tempParameters = new Dictionary<string, string> {{"search", inGroupName}};
+            var result = ExecuteQuery("groups", tempParameters, Method.GET);
+
+            //deserialize response
+            var groupList = new JsonDeserializer().Deserialize<RootGitLabGroup>(result).Property1.ToList();
+            var group = groupList.FirstOrDefault(obj => obj.name.ToUpper() == inGroupName.ToUpper());
+            if (group == null)
+            {
+                throw new Exception(string.Format("Group {0} was not found on gitlab server {1}", inGroupName,
+                    GitLabAddress));
+            }
+            return group;
+        }
+
+        public List<UserPerGroup> GetAllUsersForGroup(int inGroupId)
         {
             //GET / groups /:id / members
-            var tempParameters = new Dictionary<string, string> ();
-            var result = ExecuteQuery($"groups//members", tempParameters, Method.GET);
+            var tempParameters = new Dictionary<string, string>();
+            var result = ExecuteQuery($"groups/{inGroupId}/members", tempParameters, Method.GET);
+
+            //deserialize response
+            return new JsonDeserializer().Deserialize<RootUserPerGroup>(result).Property1.ToList();
+
+        }
+
+        private int GetUserId(List<GitLabUsers> inLabUsers ,string inUserName)
+        {
+            try
+            {
+                return inLabUsers.FirstOrDefault(obj => obj.username.ToUpper() == inUserName.ToUpper()).id;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(
+                    string.Format(
+                        "User {0} could not be found in the list of users available on the gitlab instance {1}",
+                        inUserName, GitLabAddress));
+            }
+        }
+
+        public void UpdateGroup(string inGroupName)
+        {
+            //get group
+            var tempGroup = GetProjectGroupId(inGroupName);
+            var allUsers = GetAllUsersForGroup(tempGroup.id);
+
+            //get all users
+            var tempAllUsersOnServer = GetAllUsers();
+
+            //foreach (var groupMemberOperation in newRights)
+            //{
+            //    var tempParameters = new Dictionary<string, string>();
+            //    tempParameters.Add("user_id", GetUserId(tempAllUsersOnServer, groupMemberOperation.member).ToString());
+            //    if (groupMemberOperation.OperationType != GroupMemberOperationType.Remove)
+            //    {
+            //        tempParameters.Add("access_level", groupMemberOperation.AccessLevel.ToString());
+            //    }
+            //}
         }
     }
 }
